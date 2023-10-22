674afc2f1be717512a7b1cc4022bdbf137e19421
IcyMichiko <96235160+IcyBlue17@users.noreply.github.com>
2023-10-22 12:09:20 +0800

    Create jump-to.js

---
+++ js/jump-to.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("JumpToClientShop").addEventListener("click", function() {
+         window.location.href = "https://www.baidu.com";
+     });
+    // //别问我为什么跳转也要写个js
+    // //因为我无聊
+    // //而且这样在Github仓库的语言构成多一点哈哈
+});
